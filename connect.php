d23ac027d3aee0c7491e9ad6fd1c3feb6aa6c825
ceq4l <ceq4lDev@gmail.com>
2024-11-01 21:21:05 +1030

    update

--- connect.php
+++ connect.php
@@ -3,4 +3,15 @@
     $password = $_POST['password'];
 
     //Database Connection
+    $conn = new mysqli("localhost","root","ceq4lSite");
+    if($conn->connect_error){
+        die('Connection Failed : '.$conn->connect_error);
+    }else{
+        $stmt = $conn->prepare("insert into login");
+        $stmt->bind_param("ss",$username, $password);
+        $stmt->execute();
+        echo "login successful";
+        $stmt->close();
+        $conn->close();
+    }
 ?>
